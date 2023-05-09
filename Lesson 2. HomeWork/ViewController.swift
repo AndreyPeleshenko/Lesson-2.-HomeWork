f16898350e1a499bc0af56887d0087120107c3c8
Andrey <andrey-gezet@mail.ru>
2023-05-09 19:48:01 +0300

    Initial Commit

---
+++ Lesson 2. HomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 2. HomeWork
+//
+//  Created by Андрей Гезет on 09.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
